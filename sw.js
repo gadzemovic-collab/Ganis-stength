564caf510b0da8a2e2febf38af12af968a4ae03b
gadzemovic-collab <gadzemovic@yahoo.com>
2026-09-08 22:15:57 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,42 @@
-const CACHE='gani-strength-v2-4';
+const CACHE='gani-strength-v5';
 
 const ASSETS=[
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-180.png',
-  './icon-192.png',
-  './icon-512.png'
+'./',
+'./index.html',
+'./manifest.webmanifest',
+'./icon-180.png',
+'./icon-192.png',
+'./icon-512.png'
 ];
 
 self.addEventListener('install',e=>
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(ASSETS))
-  )
+e.waitUntil(
+caches.open(CACHE).then(c=>c.addAll(ASSETS))
+)
 );
 
 self.addEventListener('activate',e=>
-  e.waitUntil(
-    caches.keys().then(keys=>
-      Promise.all(
-        keys
-          .filter(k=>k!==CACHE)
-          .map(k=>caches.delete(k))
-      )
-    ).then(()=>self.clients.claim())
-  )
+e.waitUntil(
+caches.keys().then(keys=>
+Promise.all(
+keys
+.filter(k=>k!==CACHE)
+.map(k=>caches.delete(k))
+)
+).then(()=>self.clients.claim())
+)
 );
 
 self.addEventListener('fetch',e=>
-  e.respondWith(
-    fetch(e.request)
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(e.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(e.request))
-  )
+e.respondWith(
+fetch(e.request)
+.then(response=>{
+const copy=response.clone();
+caches.open(CACHE).then(
+cache=>cache.put(e.request,copy)
+);
+return response;
+})
+.catch(()=>caches.match(e.request))
+)
 );
